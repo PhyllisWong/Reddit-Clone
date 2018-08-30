561e8b7e9ff5f4d474f88a97213900c43169b663
Phyllis Wong <wong.phyllism@gmail.com>
2018-08-30 11:54:32 -0700

    fix middleware and template engine bug

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const handlebars = require('handlebars')
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: true}));
+app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+
+// Set up a static public directory
+app.use(express.static('./public'));
+
+
+// Route / Controller
+app.get('/', (req, res) => res.render('index'));
+app.listen(3020, () => console.log('Example app listening on port 3020!'))
