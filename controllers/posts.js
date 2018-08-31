8423400b6051c9f025f93a2b5c7e6ffd6420ac9f
Phyllis Wong <wong.phyllism@gmail.com>
2018-08-31 13:18:52 -0700

    Posts are saving in the db

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,17 @@
+const Post = require('../models/posts.js');
+
 module.exports = (app) => {
 	// CREATE
 	app.post('/posts/new', (req, res) => {
+
+		// INSTANTIATE INSTANCE OF POST MODEL
+		let post = new Post(req.body);
 		console.log(req.body);
+
+		// SAVE INSTANCE OF POST MODEL TO DB
+		post.save((err, post) => {
+			// REDIRECT TO THE ROOT
+			return res.redirect('/');
+		})
 	});
 };
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
+// Port
+const port = process.env.PORT || 3000;
+
 // EXPRESS
 const app = express();
 
@@ -20,10 +23,19 @@ app.set('view engine', 'hbs');
 app.use(express.static('./public'));
 
 
+// Mongoose Connection
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/reddit-clone";
+mongoose.connect(
+	mongoUri, { useNewUrlParser: true }
+);
+
+
 // ROUTES
 const routes = require('./controllers/index');
 app.use('/', routes);
 require('./controllers/posts.js')(app);
 
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => {
+	console.log('Example app listening on port 3000!');
+});
