d39d02a38a9f39ebd5c17fc71429f1f3aea7b1ac
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-13 12:13:26 -0700

    Users can create accounts

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users');
 
 
 // Render the signup form
 router.get('/sign-up', (req, res) => {
   res.render('sign-up');
+});
+
+
+// POST: creates a new user
+router.post('/sign-up', (req, res) => {
+  const user = new User(req.body);
+
+  user.save().then( user => {
+    res.redirect('/');
+  }).catch( err => {
+    console.log(err.message);
+  })
 });
 
 
